49743b3a9056e497f0c328c6f5593426a5ca32dd
Chrisbot <chris@chrisbot.com>
2016-07-24 16:11:21 -0400

    Version 1.4 * Fixed players not spawning at X + 0.5 and Z + 0.5 of the target location. * Added config.yml option 'spawn_height' to enable specifying height above block player should spawn at target location. * Added updating timestamp of player's home yml file on player join in order to record last join. This enables easily identifying old player files. * Added subcommands to bhome: 'show' and 'tp' with tab-completion to allow ops to list and teleport to offline player homes. * Updated 'sethome' command so that if no home exists for the calling player and no arguments are given, a defaullt home 'home' is created. * Updated 'home' command so that if a calling player has only one home, that home is select and teleported to by default. * Fixed problem with locations not calculating right having to do with offsets from negative coords.

--- src/main/java/io/github/redstonefiend/bhome/commands/Home.java
+++ src/main/java/io/github/redstonefiend/bhome/commands/Home.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2015 Chris Courson.
+ * Copyright 2016 Chris Courson.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,7 +34,7 @@ import org.bukkit.entity.Player;
 
 /**
  *
- * @author chrisbot
+ * @author Chris Courson
  */
 public class Home implements CommandExecutor {
 
@@ -42,27 +42,32 @@ public class Home implements CommandExecutor {
 
     public Home(Main plugin) {
         this.plugin = plugin;
-        plugin.getCommand("home").setTabCompleter(new TabComplete(plugin));
+        plugin.getCommand("home").setTabCompleter(new PlayerTabComplete(plugin));
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "ERROR: home cannot be called from console.");
+            sender.sendMessage(ChatColor.RED + "ERROR: 'home' cannot be called from console.");
             return true;
         }
         Player player = (Player) sender;
-        if (args.length < 1) {
-            plugin.printHomes(player);
+        if ((args.length < 1) && (plugin.homes.get(player.getUniqueId()).size() > 1)) {
+            this.plugin.printHomes(player);
             return false;
         }
         if (args.length > 1) {
             player.sendMessage(ChatColor.RED + "Error in command.");
             return false;
         }
-        String homeName = args[0].toLowerCase();
-        if (plugin.homes.get(player.getUniqueId()).containsKey(homeName)) {
-            Location location = plugin.homes.get(player.getUniqueId()).get(homeName).add(0.5, 0, 0.5);
+        String homeName;
+        if (args.length == 1) {
+            homeName = args[0].toLowerCase();
+        } else {
+            homeName = plugin.homes.get(player.getUniqueId()).keySet().iterator().next();
+        }
+        if ((this.plugin.homes.get(player.getUniqueId())).containsKey(homeName)) {
+            Location location = ((Location) (this.plugin.homes.get(player.getUniqueId())).get(homeName)).clone().add(0.5D, plugin.getConfig().getDouble("spawn_height", 0.5D), 0.5D);
             if (location != null) {
                 if (player.getVehicle() != null) {
                     Entity entity = player.getVehicle();
@@ -71,13 +76,15 @@ public class Home implements CommandExecutor {
                     player.teleport(location);
                     entity.setPassenger(player);
                 } else {
-                    player.setFallDistance(0);
+                    player.setFallDistance(0.0F);
                     player.teleport(location);
                 }
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.teleportMessage));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("teleport_message", "")));
             } else {
-                plugin.printHomes(player);
+                this.plugin.printHomes(player);
             }
+        } else {
+            player.sendMessage(ChatColor.RED + "Home '" + homeName + "' not found.");
         }
         return true;
     }
