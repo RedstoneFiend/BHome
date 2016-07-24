62b9bba91752c9c37649b4f3f12eda09f531044b
Chrisbot <chris@chrisbot.com>
2016-07-24 16:11:21 -0400

    Version 1.4 * Fixed players not spawning at X + 0.5 and Z + 0.5 of the target location. * Added config.yml option 'spawn_height' to enable specifying height above block player should spawn at target location. * Added updating timestamp of player's home yml file on player join in order to record last join. This enables easily identifying old player files. * Added subcommands to bhome: 'show' and 'tp' with tab-completion to allow ops to list and teleport to offline player homes.

--- src/main/java/io/github/redstonefiend/bhome/commands/BHome.java
+++ src/main/java/io/github/redstonefiend/bhome/commands/BHome.java
@@ -24,32 +24,89 @@
 package io.github.redstonefiend.bhome.commands;
 
 import io.github.redstonefiend.bhome.Main;
+import java.util.Map;
+import java.util.UUID;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 /**
  *
  * @author Chris Courson
  */
-public class BHome
-        implements CommandExecutor {
+public class BHome implements CommandExecutor {
 
     private final Main plugin;
 
     public BHome(Main plugin) {
         this.plugin = plugin;
+        plugin.getCommand("bhome").setTabCompleter(new BHomeTabComplete(plugin, this));
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if ((args.length == 0) || ((args.length == 1) && ((args[0].equalsIgnoreCase("ver")) || (args[0].equalsIgnoreCase("version"))))) {
+
             sender.sendMessage(ChatColor.GOLD + "Boomerang Home " + ChatColor.GREEN + "version " + ChatColor.YELLOW + this.plugin.getDescription().getVersion());
+
         } else if ((args.length == 1) && (args[0].equalsIgnoreCase("reload"))) {
+
             this.plugin.reloadConfig();
             sender.sendMessage(ChatColor.YELLOW + "BHome config reloaded.");
+
+        } else if ((args.length >= 2) && (args[0].equalsIgnoreCase("show") || args[0].equalsIgnoreCase("tp"))) {
+
+            UUID playerID = getPlayerId(args[1]);
+            if (playerID == null) {
+                sender.sendMessage(ChatColor.RED + "Player '" + args[1] + "' not found.");
+                return true;
+            }
+
+            Map<String, Location> homes = plugin.loadPlayerHomes(playerID, false);
+
+            if (args.length == 2) {
+                if (homes.isEmpty()) {
+                    sender.sendMessage(String.format(ChatColor.GOLD + "Player '%s' has no homes set.",
+                            plugin.getServer().getOfflinePlayer(playerID).getName()));
+                } else {
+                    sender.sendMessage(String.format(ChatColor.GOLD + "Homes for '%s' (%d): %s", new Object[]{
+                        plugin.getServer().getOfflinePlayer(playerID).getName(), homes.size(), homes.keySet().toString().replaceAll("[\\[\\]]", "")}));
+                }
+            } else {
+                if (homes.containsKey(args[2].toLowerCase())) {
+
+                    Location location = homes.get(args[2].toLowerCase()).clone().add(0.5D, plugin.getConfig().getDouble("spawn_height", 0.5D), 0.5D);
+
+                    if (args[0].equalsIgnoreCase("show")) {
+                        sender.sendMessage(String.format(ChatColor.GOLD + "Home '%s' for '%s' is located at:\n  X(%d) Y(%d) Z(%d)", new Object[]{
+                            args[2].toLowerCase(), plugin.getServer().getOfflinePlayer(playerID).getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ()}));
+                    } else {
+                        Player callingPlayer = (Player) sender;
+                        if (callingPlayer.getVehicle() != null) {
+                            Entity entity = callingPlayer.getVehicle();
+                            entity.eject();
+                            entity.teleport(location);
+                            callingPlayer.teleport(location);
+                            entity.setPassenger(callingPlayer);
+                        } else {
+                            callingPlayer.setFallDistance(0.0F);
+                            callingPlayer.teleport(location);
+                        }
+                        callingPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', this.plugin.getConfig().getString("teleport_message", "")));
+                    }
+                } else {
+                    sender.sendMessage(String.format(ChatColor.RED + "Home '%s' for '%s' not found.", new Object[]{
+                        args[2].toLowerCase(), plugin.getServer().getOfflinePlayer(playerID).getName()}));
+                }
+            }
+
         } else if ((args.length == 2) && (args[0].equalsIgnoreCase("max"))) {
+
             int max;
             try {
                 max = Integer.parseInt(args[1]);
@@ -60,8 +117,11 @@ public class BHome
             this.plugin.getConfig().set("max_homes", max);
             this.plugin.saveConfig();
             sender.sendMessage(ChatColor.GREEN + "Max homes saved.");
+
         } else if ((args.length == 1) && (args[0].equalsIgnoreCase("max"))) {
+
             sender.sendMessage(ChatColor.GREEN + "Max homes = " + Integer.toHexString(this.plugin.getConfig().getInt("max_homes", 5)) + ".");
+
         } else {
             return false;
         }
@@ -70,5 +130,16 @@ public class BHome
         } else {
             return true;
         }
+    }
+
+    UUID getPlayerId(String playerName) {
+        UUID playerID = null;
+        OfflinePlayer[] offlinePlayers = plugin.getServer().getOfflinePlayers();
+        for (OfflinePlayer offlinePlayer : offlinePlayers) {
+            if (offlinePlayer.getName().equalsIgnoreCase(playerName)) {
+                playerID = offlinePlayer.getUniqueId();
+            }
+        }
+        return playerID;
     }
 }
